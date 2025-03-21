b14c252fc1285d3761ebe218d06b31395a7b263a
Ware, Joseph (DLSLtd,RAL,LSCI) <joseph.ware@diamond.ac.uk>
2025-03-21 10:43:12 +0000

    Maintain tested behaviour

--- src/test_joseph/cli.py
+++ src/test_joseph/cli.py
@@ -7,7 +7,7 @@ from . import __version__
 
 
 @click.group()
-@click.version_option(version=__version__, prog_name="joseph")
+@click.version_option(version=__version__, message="%(version)s")
 def main() -> None: ...
 
 
